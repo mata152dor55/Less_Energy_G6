97efd8a9e46356493f439116a4e861912a75b407
Santiago <santiagocolonio@gmail.com>
2026-04-10 11:49:35 -0500

    Initial commit

---
+++ src/main/java/com/example/less_energy_g6/LessEnergyG6Application.java
@@ -0,0 +1,13 @@
+package com.example.less_energy_g6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessEnergyG6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessEnergyG6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/less_energy_g6/LessEnergyG6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.less_energy_g6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessEnergyG6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
